29d437526376061b7978e72a0566150c2c6d2971
Denis Dementev <dementievdo170792@gmail.com>
2025-10-21 07:42:09 +0300

    HW03 corrections after review

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,11 +1,9 @@
 DROP TABLE IF EXISTS user_role;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS meals;
 DROP SEQUENCE IF EXISTS global_seq;
-DROP SEQUENCE IF EXISTS global_seq2;
 
 CREATE SEQUENCE global_seq START WITH 100000;
-CREATE SEQUENCE global_seq2 START WITH 100000;
 
 CREATE TABLE users
 (
@@ -23,18 +21,17 @@ CREATE TABLE user_role
 (
     user_id INTEGER NOT NULL,
     role    VARCHAR NOT NULL,
-    CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+    CONSTRAINT user_unique_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 CREATE TABLE meals
 (
-    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq2'),
+    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
     date_time   TIMESTAMP                         NOT NULL,
     description VARCHAR                           NOT NULL,
     calories    INTEGER                           NOT NULL,
     user_id     INTEGER                           NOT NULL,
-    CONSTRAINT user_meals_idx UNIQUE (id, date_time),
+    CONSTRAINT meals_unique_user_id_date_time_idx UNIQUE (user_id, date_time),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -2,7 +2,6 @@ DELETE FROM user_role;
 DELETE FROM users;
 DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
-ALTER SEQUENCE global_seq2 RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', 'password'),
@@ -15,5 +14,11 @@ VALUES ('USER', 100000),
 
 INSERT INTO meals (date_time, description, calories, user_id)
 VALUES ('2022-10-30 21:00', 'ужин', 1000, 100001),
+       ('2022-10-29 10:00', 'обед', 1300, 100001),
+       ('2022-10-29 13:00', 'обед', 500, 100001),
+       ('2022-10-29 19:00', 'обед', 500, 100001),
        ('2022-10-31 9:00', 'завтрак', 500, 100000),
-       ('2022-10-29 12:00', 'обед', 300, 100001);
+       ('2022-10-31 12:00', 'обед', 500, 100000),
+       ('2022-10-31 21:00', 'ужин', 1200, 100000),
+       ('2022-10-30 12:00', 'обед', 500, 100000),
+       ('2022-10-30 21:00', 'ужин', 1200, 100000);
